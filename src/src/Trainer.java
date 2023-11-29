9a5de338c01f66fadc7aceed4bf92ce585fbd521
zaaz <zaaz@itu.dk>
2023-11-29 12:36:22 +0100

    Remade functionality

--- src/src/Trainer.java
+++ src/src/Trainer.java
@@ -1,6 +1,8 @@
+import comparator.SwimmingTimeComparator;
 import member.Member;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class Trainer {
 
@@ -23,8 +25,10 @@ public class Trainer {
             if(member.getIsCompeting() || member.getActivity().equals("competing")) {
                 if (memberAge >= adultAge && !competingAboveAge.contains(member)) {
                     competingAboveAge.add(member);
+                    Collections.sort(competingAboveAge, new SwimmingTimeComparator());
                 } else if(memberAge < adultAge && !competingUnderAge.contains(member)){
                     competingUnderAge.add(member);
+                    Collections.sort(competingUnderAge, new SwimmingTimeComparator());
                 }
             }
         }
@@ -33,14 +37,26 @@ public class Trainer {
     public void displayMembersWithTrainer(){
         divideMembers();
 
-        System.out.println("Trainer 1:");
+        System.out.println("Senior:");
         for(Member member : competingAboveAge){
-            System.out.println(member.getName() + " , " + member.getAge() + " , " + member.getActivity());
+            System.out.println(member.getName() + " , " + member.getAge() + " , " + member.getActivity() + " , " + member.getSwimmingTime());
         }
 
-        System.out.println("Trainer 2:");
+        System.out.println("Junior:");
         for(Member member : competingUnderAge){
-            System.out.println(member.getName() + " , " + member.getAge() + " , " + member.getActivity());
+            System.out.println(member.getName() + " , " + member.getAge() + " , " + member.getActivity() + " , " + member.getSwimmingTime());
+        }
+    }
+
+    public void displayTopFiveSwimmers(){
+        int count = 0;
+        Collections.sort(controller.getMemberList(), new SwimmingTimeComparator());
+        for(Member member : controller.getMemberList()){
+            count++;
+            System.out.println(member.getName() + " , " + member.getAge() + " , " + member.getSwimmingTime());
+            if(count >= 5){
+                break;
+            }
         }
     }
 }
